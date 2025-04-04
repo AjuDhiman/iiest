48aaf00e888dce8f2fb87915686b119d1e10d526
rutuja-ri <rutuja.s@riequation.com>
2025-04-04 15:43:41 +0530

    added notifiaction popup

--- iiest_web/src/app/shared/consumer-footer/consumer-footer.component.ts
+++ iiest_web/src/app/shared/consumer-footer/consumer-footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faShoppingCart, faBell, faPhone, faChalkboardTeacher, faBars ,faBookOpen ,faPhoneVolume
 } from '@fortawesome/free-solid-svg-icons';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-consumer-footer',
@@ -12,13 +14,25 @@ import { faShoppingCart, faBell, faPhone, faChalkboardTeacher, faBars ,faBookOpe
 export class ConsumerFooterComponent {
   
   phoneNumber: string = '9289310979'; 
+  driveVideoUrl: string = 'https://drive.google.com/file/d/1isI_YuozFcDhLjV4IVgfmVCFVnrILZFx/view?usp=drive_link';
+  trustedVideoUrl: SafeResourceUrl | null = null;
+  constructor(library: FaIconLibrary,private router: Router,private sanitizer: DomSanitizer) {
+    this.trustedVideoUrl = this.getEmbedUrl(this.driveVideoUrl);
 
-  constructor(library: FaIconLibrary,private router: Router) {
     library.addIcons(faShoppingCart, faBell, faPhone, faChalkboardTeacher, faBars,faBookOpen,faPhoneVolume);
   }
 
   navigateToOtherOptions() {
     this.router.navigate(['/consumer-other-option']);
+  }
+  getEmbedUrl(url: string): SafeResourceUrl | null {
+    const match = url.match(/\/d\/([a-zA-Z0-9_-]+)/);
+    if (match && match[1]) {
+      const fileId = match[1];
+      const previewUrl = `https://drive.google.com/file/d/${fileId}/preview`;
+      return this.sanitizer.bypassSecurityTrustResourceUrl(previewUrl);
+    }
+    return null;
   }
   navigateToNotification() {
     this.router.navigate(['/consumer-notification']);
@@ -31,4 +45,29 @@ export class ConsumerFooterComponent {
   makeCall() {
     window.location.href = 'tel:' + this.phoneNumber;
   }
+  openNotificationModal() {
+    const modalElement = document.getElementById('notificationModal');
+    if (modalElement) {
+      const modal = new Modal(modalElement, { backdrop: false });
+      modal.show();
+  
+      // Handle animation on close
+      modalElement.addEventListener('hide.bs.modal', (event) => {
+        const dialog = modalElement.querySelector('.modal-dialog');
+        if (dialog) {
+          // Add slide-out class
+          dialog.classList.add('slide-out-right');
+  
+          // Delay actual hide
+          event.preventDefault(); // prevent instant hide
+  
+          setTimeout(() => {
+            modal.hide(); // force hide after animation
+            dialog.classList.remove('slide-out-right');
+          }, 400); // match CSS animation duration
+        }
+      }, { once: true }); // only once per modal open
+    }
+  }
+  
 }
