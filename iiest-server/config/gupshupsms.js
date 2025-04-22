660eecfe71b03951d5c75c704d1a48b51f1e69ac
rutuja-ri <rutuja.s@riequation.com>
2025-04-22 10:04:11 +0530

    added messages in bo regiter

--- iiest-server/config/gupshupsms.js
+++ iiest-server/config/gupshupsms.js
@@ -14,22 +14,30 @@ const qs = require('qs')
 // methord for sending bo verification sms
 // manager_contact
 
-
 async function shortenURLWithTinyURL(longUrl) {
     try {
       const response = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(longUrl)}`);
-      return response.data; // shortened URL
+      let shortUrl = response.data;
+  
+      // Remove protocol (http:// or https://) if present
+      shortUrl = shortUrl.replace(/^https?:\/\//, '');
+  
+      return shortUrl; // cleaned URL without protocol
     } catch (error) {
       console.error('TinyURL Error:', error.message);
       throw error;
     }
   }
+  
 exports.sendBOVerificationSMS = async (_id, email, phoneNo) => {
     const verifyLink = `${FRONT_END.VIEW_URL}#/verifyonboard/bo/${_id}`;
     const dltTempletID = '1007928778645796672';
      const shortUrl = await shortenURLWithTinyURL(verifyLink);
-    const message = `Thanks for registering in the Connect Bharat. To complete the registration process and activate your account, Please verify your email address ${email} and your phone number ${phoneNo}.by clicking the link below. ${shortUrl}For any query call us on 9289310979 - Connect Bharat (IIEST)`;
-    
+     const decodedShortUrl = decodeURIComponent(shortUrl);
+
+    // const message = `Thanks for registering in the Connect Bharat. To complete the registration process and activate your account, Please verify your email address ${email} and your phone number ${phoneNo}.by clicking the link below. ${shortUrl}For any query call us on 9289310979 - Connect Bharat (IIEST)`;
+    const message = `Thanks for registering in the Connect Bharat. To complete the registration process and activate your account, Please verify your email address ${email} and your phone number ${phoneNo}.by clicking the link below. {var} For any query call us on 9289310979 - Connect Bharat (IIEST)`;
+    console.log("aaaa",message);
     await sendRapidoSMS(message, phoneNo, dltTempletID);
 
 
